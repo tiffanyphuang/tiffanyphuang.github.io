3de87daa1c77c0a708c73e9af9091f4cf832c155
Tiffany Huang <tiffanyhuang@MacBook-Pro-4.local>
2023-04-13 15:47:27 -0700

    finished hw8

---
+++ homeworks/hw8/assets/js/script.js
@@ -0,0 +1,6 @@
+function toggleSongs() {
+    var songsSecondHalf = document.getElementById('songs-second-half');
+    songsSecondHalf.classList.toggle('hidden');
+  }
+
+document.getElementById("toggle-button").onclick = toggleSongs;
